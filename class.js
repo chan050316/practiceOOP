3030420d92253b061dbc099e61aea0d0978dc611
chan-kim <chan.ckeem@gmail.com>
2022-10-06 00:00:37 +0900

    C how to inheritance class / object

--- class.js
+++ class.js
@@ -37,3 +37,45 @@ class Person {
 const kim = new Person("kim", 20, 30);
 
 console.log(kim.sum());
+
+// inheritance
+// 위에 Person의 class에 평균을 구하는 method인 avg()를 추가하고 싶다.
+// method는 아래의 두가지 방법으로도 추가할 수 있다.
+// 1. class 자체에 method를 더해 수정하는 것 => class가 자신이 만든것이 아니라면 불가능
+// 2. class 하나를 더 만든다 => 비효율적
+// 원래 만들어져 있는 class에 method를 추가하는 것과 동시에, 코드의 중복을 줄이게 해주는 것이 바로 상속
+
+class PersonPlus extends Person {
+  // PersonPlus는 Person의 확장판이다 / Person을 상속한다
+  avg() {
+    return (this.first + this.second) / 2;
+  }
+}
+
+// 상속했을 때 일어날 수 있는 문제점
+// 만약 PersonPlus에서만 third라는 인자를 받고 싶을 때는 어떻게 해야 하는가
+// 그러면 부모class를 먼저 작동한 뒤에 부모class가 하지 못하는 일을 자식 class가 하게 할 수 있다.
+// super를 사용
+
+class PersonPlus extends Person {
+  constructor(name, first, second, third) {
+    // 사용할 파라미터를 인자로 모두 넣어준다.
+    super(name, first, second); // super()는 부모의 constructor를 그대로 가져와 실행한다.
+    this.third = third;
+  }
+  sum() {
+    return super.sum() + this.third;
+  }
+  avg() {
+    return (this.first + this.second + this.third) / 3;
+  }
+}
+
+// super()는 부모 class의 constructor를 불러와 실행시킨다
+// super.은 부모 class 자체를 불러와 부모 class의 method를 실행할 수 있게 한다.
+
+// 다른 객체지향언어와 javascript의 차이점
+// javascript는 객체지향언어지만 prototype based language이기에 class와 class로 만들어진 object와의 관계가 다른 객체지향언어들과는 다르게 이루어진다.
+// 일반 객체지향언어 === Main class => Sub class => object
+// javascript === Main object(prototype object) <(prototype link)= Sub object [javascript는 object에서 상속이 이루어진다.]
+// 주의 : 우리가 썼던 class나 super는 javascript 이용자의 편의를 위해 만들어진 명령어일뿐 실행되는 동장은 javascript에 기초되어 있어 다른 객체지향언어들과는 다르게 작동된다.
---
+++ prototype-inheritance.js
@@ -0,0 +1,35 @@
+// class를 이용하지 않는 javascript에서의 상속과정 ) 1
+const superObj = {
+  superVal: "super",
+};
+const subObj = {
+  subVal: "sub",
+};
+// 아직 둘은 아무 관계도 아니다. 하지만 javascript는 쉽게 이 둘을 부모 자식 관계로 만들 수 있다.
+// __proto__를 이용해 subObj의 프로토타입 링크(상속 받을 링크)를 지정해주면 된다.
+
+subObj.__proto__ = superObj;
+console.log(subObj.subVal); // sub
+console.log(subObj.superVal); // super
+// 부모자식 관계가 만들어져 subObj에서도 superVal를 가져올 수 있게 되었다.
+
+// subObj.superVal = "sub";
+// console.log(subObj.superVal); // sub
+// console.log(superObj.superVal); // super
+// 그리고 상속된 값을 이후에 바꾼다해도 부모의 값은 바뀌지 않는다.
+// 그리고 이 __proto__는 언제든 수정해 링크를 바꿀 수도 있다 (유연하지만 좋은 것만은 아니다)
+// 또 __proto__는 생긴게 이상한것 처럼 javascript에선 표준으로 인정하고 있지 않다. == 이것은 정석은 아니다.
+
+// class를 이용하지 않는 javascript에서의 상속과정 ) 2
+
+// const superObj = {
+//   superVal: "super",
+// };
+const subObj = Object.create(superObj);
+// superObj 안에 subObj라는 새로운 객체를 만든다. === 자세히 말하자면 subObj의 프로토타입링크를 설정해주는 것
+subObj.subVal = "sub";
+// 그 이후 객체 안에 값을 넣어준다
+// debugger;
+// 웹 검사 source에서 본다면 코드를 여기서 멈춘 뒤 실행되는 코드에 대해 자세히 볼 수 있다.
+console.log(subObj.subVal); // sub
+console.log(subObj.superVal); // super
